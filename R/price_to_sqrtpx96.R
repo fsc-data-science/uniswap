143f53df5851c956795989559df88848e0cd1a9a
Carlos R. Mercado <107061601+charlieflipside@users.noreply.github.com>
2023-06-21 10:56:30 -0400

    Prices Back/Forth Documented

--- R/price_to_sqrtpx96.R
+++ R/price_to_sqrtpx96.R
@@ -1,18 +1,32 @@
 #' Price to SQRTPX96
 #'
 #' Uniswap stores prices as square roots in 64.96 (64 bits integer, 96 bit fractional)
-#' assume sqrt price is a rational and use gmp big integer
+#' assume sqrt price is a rational and use gmp big integer.
 #'
-#' @param P price in standard form (e.g., 0.05 BTC/ETH). Be aware of the pool specific
-#' token 0 and token 1 as this function takes your number as is.
+#' @param P price in human readable form (e.g., 0.05 BTC/ETH).
+#' @param invert Default FALSE. Uniswap uses Token 1 / Token 0. You must know which token is which at the pool level.
+#' @param decimal_adjustment 10^(decimal difference). WBTC has 8 decimals, ETH has 18. See example.
 #'
-#' @return Big Integer price in sqrtpx96 format.
+#' @return Big Integer price in sqrtpx96 format. Note, small amount of
+#' precision loss (<0.01%) possible due to not adjusting for Solidity implementation of
+#' Chinese Remainder Theorem as solidity uses fixed point math.
+#' @import gmp
 #' @export
 #'
 #' @examples
-#' price_to_sqrtpx96(0.05) == gmp::as.bigq('17715955711429570570949230592')
-price_to_sqrtpx96 <- function(P){
+#' # see the reverse function ?sqrtpx96_to_price
+#' # For Ethereum Mainnet ETH-USDC 0.05% v3 Pool example:
+#' # $1,825.732 USDC/ETH -> 1854219362252931989533640458424264 (Slot0 in Pool Contract)
+#' # invert is TRUE because pool is actually ETH/USDC (Token 1 / Token 0) NOT USDC/ETH.
+#' # USDC is 6 decimals while ETH is 18 decimals (18-6 = 12 decimal_adjustment)
+#' price_to_sqrtpx96(1825.732, invert = TRUE, decimal_adjustment = 1e12)
+#' # 99.99999% accurate. Some precision loss compared to fixed point math in solidity.
+price_to_sqrtpx96 <- function(P, invert = FALSE, decimal_adjustment = 1e0){
 
-    gmp::as.bigq(sqrt(P)) * gmp::as.bigz(2)^96
+  if(invert){
+    P <- P^-1
+  }
+
+  gmp::as.bigq(sqrt(P*decimal_adjustment)) * gmp::as.bigz(2)^96
 
 }
--- R/sqrtpx96_to_price.R
+++ R/sqrtpx96_to_price.R
@@ -3,20 +3,29 @@
 #' assume sqrt price is a rational and use gmp big integer.
 #'
 #' @param sqrtpX96 The Uniswap 64.96 square root price to convert to human readable price.
-#' Note: Uniswap pools always use Token 0 / Token 1 pricing. So invert result as you see fit.
+#' @param invert Default FALSE. Uniswap uses Token 1 / Token 0. You must know which token is which at the pool level.
+#' @param decimal_adjustment 10^(decimal difference). WBTC has 8 decimals, ETH has 18. See example.
 #'
-#' @return Human readable decimal price (Token 0 / Token 1 format). Note, small amount of
+#' @return Human readable decimal price in desired format (1/0 or 0/1 if invert = TRUE). Note, small amount of
 #' precision loss (<0.01%) possible due to not adjusting for Solidity implementation of
 #' Chinese Remainder Theorem as solidity uses fixed point math.
 #' @export
 #'
 #' @examples
-#' # returns FALSE b/c of precision.
-#' sqrtpx96_to_price('17715955711429570570949230592') == 0.05
-#' # but difference is < 1e-15
-#' abs(sqrtpx96_to_price('17715955711429570570949230592') - 0.05) < 1e-15
-#' # see ?price_to_sqrtpx96
-sqrtpx96_to_price <- function(sqrtpX96){
+#' # see the reverse function ?price_to_sqrtpx96
+#' # For Ethereum Mainnet ETH-USDC 0.05% v3 Pool example:
+#' # 1854219362252931989533640458424264 (Slot0 from Pool Contract) -> $1,825.732 USDC/ETH
+#' # invert is TRUE because pool is actually ETH/USDC (Token 1 / Token 0) NOT USDC/ETH.
+#' # USDC is 6 decimals while ETH is 18 decimals (18-6 = 12 decimal_adjustment)
+#' sqrtpx96_to_price('1854219362252931989533640458424264', invert = TRUE, decimal_adjustment = 1e12)
+#' # 99.99999% accurate. Some precision loss compared to fixed point math in solidity.
+sqrtpx96_to_price <- function(sqrtpX96, invert = FALSE, decimal_adjustment = 1e0){
   p <- gmp::as.bigq(sqrtpX96)/(gmp::as.bigz(2)^96)
-  as.numeric(p^2)
+
+  if(invert){
+  as.numeric(p^2)^(-1) * decimal_adjustment
+  } else {
+  as.numeric(p^2) / decimal_adjustment
+  }
+
 }
