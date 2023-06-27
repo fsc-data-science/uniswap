ad5155c3eae32a1b893b9f9d7f0750bd0825b26f
Carlos R. Mercado <107061601+charlieflipside@users.noreply.github.com>
2023-06-27 15:58:30 -0400

    full rewrite of helpers to fit price<->tick upgrade

--- R/check_positions.R
+++ R/check_positions.R
@@ -4,20 +4,23 @@
 #'
 #' @param ptbl Liquidity Positions table of the form tick_lower, tick_upper, liquidity. See ?ethwbtc_net_liquidity for an example.
 #' This included dataset is net_liquidity, so it should be filtered to the max blockheight for a given tick_lower-tick_upper.
-
 #' @param P Specific price in human readable format, converted to tick for flagging using ?get_closest_tick with tick_spacing = 1.
-#'
+#' @param decimal_adjustment The difference in the tokens decimals, e.g., 1e10 for ETH vs BTC. 1e12 for USDC vs ETH; 1 (default) for most ERC20/ETH.
+#' @param yx Whether price is already in Token 1 / Token 0 format or inverted. ETH per USDC is how the ETH Mainnet 0.05\% pool works but is not friendly for human interpretation. Default is TRUE.
 #' @return The liquidity positions table with a new `active` flag column for whether the liquidity is active or not at price P.
 #' @export
 #'
 #' @examples
 #' data("ethwbtc_net_liquidity")
-#' ethwbtc_net_liquidity <- check_positions(ethwbtc_net_liquidity, P = 0.10, decimal_adjustment = 1e10, yx = FALSE)
-check_positions <- function(ptbl, P , decimal_adjustment = 1e0, yx = TRUE){
+#' # note this would still need to be aggregated to row with max block for a given tick_lower-tick_upper combo (i.e. liquidity for range at a blockheight)
+#' check_positions(ethwbtc_net_liquidity, P = 0.10, decimal_adjustment = 1e10, yx = FALSE)
+
+check_positions <- function(ptbl, P, decimal_adjustment = 1e0, yx = TRUE){
   if( !("tick_lower" %in% colnames(ptbl)) | !("tick_upper" %in% colnames(ptbl))){
     stop("Expected tick_lower and tick_upper columns")
   }
 
+  # for making positions tick_spacing = 1 is only valid for 0.01 percent pools, but this works for inside a valid range
   tick_target <- uniswap::get_closest_tick(P, tick_spacing = 1, decimal_adjustment = decimal_adjustment, yx = yx)
   target_tick <- tick_target$tick
 
--- R/find_recalculation_price.R
+++ R/find_recalculation_price.R
@@ -1,24 +1,45 @@
 #' Find Recalculation Price
 #'
-#' @param positions tbd
-#' @param current_price tbd
-#' @param price_up tbd
+#' Helper function for identifying the next tick where liquidity must be recalculated (i.e., at least 1 position enters or exits being active).
+#' Note, pass both active and inactive positions with liquidity, see ?check_positions.
+#'
+#' @param ptbl Liquidity Positions table of the form tick_lower, tick_upper, liquidity. See ?ethwbtc_net_liquidity for an example.
+#' This included dataset is net_liquidity, so it should be filtered to the max blockheight for a given tick_lower-tick_upper.
+#' @param P Specific price in human readable format, converted to tick for flagging using ?get_closest_tick with tick_spacing = 1.
+#' @param price_up Is the price rising (TRUE) or falling (FALSE) given specified format for `P`. Default TRUE.
+#' @param decimal_adjustment The difference in the tokens decimals, e.g., 1e10 for ETH vs BTC. 1e12 for USDC vs ETH; 1 (default) for most ERC20/ETH.
+#' @param yx Whether price is already in Token 1 / Token 0 format or inverted. ETH per USDC is how the ETH Mainnet 0.05\% pool works but is not friendly for human interpretation. Default is TRUE.
 #'
 #' @return tbd
 #' @export
 #'
 #' @examples
-#' "tbd"
-find_recalculation_price <- function(positions, current_price, price_up = TRUE){
+#'
+find_recalculation_price <- function(ptbl, P, price_up = TRUE, decimal_adjustment = 1e0, yx = TRUE){
 
-  p <- positions[, c("min_price","max_price")]
-
-  # if price going up, get the closest available price above current price
-  if(price_up == TRUE){
-    closest_price <- p[p > current_price][which.min(p[p > current_price])]
-  } else {
-    closest_price <- p[p < current_price][which.max(p[p < current_price])]
+  if( !("tick_lower" %in% colnames(ptbl)) | !("tick_upper" %in% colnames(ptbl))){
+    stop("Expected tick_lower and tick_upper columns")
   }
+
+  # for making positions tick_spacing = 1 is only valid for 0.01 percent pools, but this works for inside a valid range
+  tick_target <- uniswap::get_closest_tick(P, tick_spacing = 1, decimal_adjustment = decimal_adjustment, yx = yx)
+
+  target_tick <- tick_target$tick
+
+  relevant_ticks <- sort(unique(c(ptbl[["tick_lower"]], ptbl[["tick_upper"]])))
+
+  # if price going up, get the closest price above current price where liquidity changes
+  # if price going down, get the closest price below current price where liquidity changes
+  if( price_up == TRUE & yx == TRUE ){
+    closest_tick <- relevant_ticks[which(relevant_ticks > target_tick)[1]]
+  } else if( price_up == FALSE & yx == TRUE ) {
+    closest_tick <- relevant_ticks[max(which(relevant_ticks < target_tick))]
+  }  else {
+    # if yx is FALSE, invert everything, re-calculate, and invert the result.
+    return(find_recalculation_price(ptbl, P^-1, !price_up, decimal_adjustment, !yx)^-1)
+  }
+
+  closest_price <- tick_to_price(closest_tick, decimal_adjustment = decimal_adjustment, yx = yx)
 
   return(closest_price)
 }
